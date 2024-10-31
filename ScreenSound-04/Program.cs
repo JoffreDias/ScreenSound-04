79c7972bef65b6ac417b092390d1b9f4d5c40c6a
JoffreDias <joffrealysson@gmail.com>
2024-10-31 18:17:24 -0300

    correção de erro de commit

--- ScreenSound-04/Program.cs
+++ ScreenSound-04/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using (HttpClient client = new HttpClient())
+{
+    client.Timeout = TimeSpan.FromMinutes(3);
+    string resposta = await client.GetStringAsync("https://joffredias.github.io/api-csharp-songs/");
+
+    ///////
+    ///////
+    Console.WriteLine(resposta);
+}
